a495d223da461c2b47761926c863e1dc6d4c2f6f
JC Phua <jcphua@gmail.com>
2020-01-16 16:28:04 +1100

    Read-process csv

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 const fs = require('fs'),
+      path = require('path'),
       readline = require('readline'),
       csvParser = require('csv-parser');
 
-console.log('hello');
+const _csv_filenames = ['test500', 'test4000', 'node-data-processing-medium-data'],
+      csv_filename = _csv_filenames[0],
+      csv_path = path.join(__dirname, `../data/${ csv_filename }.csv`);
+
+let data = [];
+
+console.log(`Using following file as data source:\n'${ csv_path }'`);
+
+fs.createReadStream(csv_path)
+    .on('error', () => {
+        console.error(arguments);
+    })
+    .pipe(csvParser())
+    .on('data', (row) => {
+        // console.log(row);
+        data.push(row);
+    })
+    .on('end', () => {
+        console.log(`${ data.length } records processed.`);
+
+        // readline
+    });
